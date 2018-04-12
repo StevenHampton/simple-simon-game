52b228ce5fbc636431235db1e8b5859fcc52f748
steven hampton <steven.hampton415@gmail.com>
2018-04-12 15:21:23 -0500

    got basic functions to work

--- simple-simon.js
+++ simple-simon.js
@@ -8,12 +8,43 @@ var id, color, level = 0;
 //1- start board sequence
 $(document).ready(function(){
     $("#start").click(function(){
+        level++;
         startSequence();
     })
 });
 
+
+//Simon sequence
 function startSequence(){
+    console.log(level);
     $("#displayCounter").text(level);
+    getRanomNum();
+    var i = 0;
+    var myInterval = setInterval(function(){
+        id = simonSeq[i];
+        color = $("#"+id).attr("class").slice(10);
+        console.log(id + " " + color);
+        addClass(id, color);
+        i++;
+        if( i === simonSeq.length){
+            clearInterval(myInterval);
+        }
+    }, 1000);
+
+}
+
+//generate random number
+function getRanomNum() {
+    var random = Math.floor(Math.random() * 4);
+    simonSeq.push(random);
+}
+
+//add temporary class
+function addClass(id, color){
+    $("#"+id).addClass(color + "-active");
+    setTimeout(function(){
+        $("#"+id).removeClass(color + "-active");
+    }, 500);
 }
 
 //2- User replicated sequence
